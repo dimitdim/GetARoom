2f9eb0c891f54e884642efba7e145d597fd83c76
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2014-04-30 19:32:40 -0400

    user implementation

--- Main/app/models.py
+++ Main/app/models.py
@@ -6,6 +6,8 @@ Model that defines the class structure of the database.
 
 from app import db
 
+ROLE_USER = 0
+ROLE_ADMIN = 1
 
 class Node(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,3 +48,31 @@ class Data(db.Model):
 
     def __repr__(self):
         return "Light: %s, Temp: %s, Last: %s" % (self.brightness, self.temperature, self.last_opened)
+
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	nickname = db.Column(db.String(64), index = True, unique = True)
+	email = db.Column(db.String(120), index = True, unique = True)
+	role = db.Column(db.SmallInteger, default = ROLE_USER)
+	posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+	
+	def is_authenticated(self):
+		return True
+	def is_active(self):
+		return True
+	def is_anonymous(self):
+		return False
+	def get_id(self):
+		return unicode(self.id)
+	def __repr__(self):
+		return '<User %r>' % (self.nickname)
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Post %r>' % (self.body)
