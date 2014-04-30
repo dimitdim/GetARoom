73141fa67bded7d28ef46b9cfe9ff93081984156
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2014-04-30 19:26:26 -0400

    views w/ login

--- Main/app/views.py
+++ Main/app/views.py
@@ -6,59 +6,65 @@ from forms import LoginForm
 from models import User, ROLE_USER, ROLE_ADMIN
 
 
-user = {'nick': 'dimitdim', 'name': 'Dimitar'}
-tim = str(time.asctime(time.localtime()))
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
 
+@app.before_request
+def before_request():
+	g.user = current_user
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    title = 'test'
-    return render_template("index.html", title=title, user=user, time=tim)
+	tim = str(time.asctime(time.localtime()))
+	user=g.user
+	title = 'test'
+	return render_template("index.html", title=title, user=user, time=tim)
 
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods = ['GET','POST'])
 @oid.loginhandler
 def login():
-    form = LoginForm()
-    title = 'Sign In'
-    providers = app.config['OPENID_PROVIDERS']
-    if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('index'))
-    if form.validate_on_submit():
-        flash('OpenID: ' + form.openid.data)
-        flash('Cookie: ' + str(form.remember_me.data))
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template("login.html", title=title, form=form, user=user, time=tim, providers=providers)
+	if g.user is not None and g.user.is_authenticated():
+		return redirect(url_for('index'))
+	tim = str(time.asctime(time.localtime()))
+	form = LoginForm()
+	title = 'Sign In'
+	user = g.user
+	providers = app.config['OPENID_PROVIDERS']
+	if form.validate_on_submit():
+		flash('OpenID: '+form.openid.data)
+		flash('Cookie: '+str(form.remember_me.data))
+		session['remember_me']=form.remember_me.data
+		return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+	return render_template("login.html", title=title, form=form, user=user, time=tim, providers=providers)
 
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
 
 @app.route('/css')
 def css():
-    return render_template("default.css")
-
-
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
+	return render_template("default.css")
 
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
+    user = User.query.filter_by(email = resp.email).first()
     if user is None:
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
         db.session.add(user)
         db.session.commit()
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
         session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
+    login_user(user, remember = remember_me)
     return redirect(request.args.get('next') or url_for('index'))
